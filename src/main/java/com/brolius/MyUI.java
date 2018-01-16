b90ddc5aadd8c8597cdb9f3b93c992a185b8a323
Gabriel Brolo <gabrolo13@gmail.com>
2018-01-15 23:48:10 -0600

    Error UX.

--- src/main/java/com/brolius/MyUI.java
+++ src/main/java/com/brolius/MyUI.java
@@ -8,8 +8,11 @@ import com.brolius.antlr.decafLexer;
 import com.brolius.antlr.decafParser;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.Position;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.*;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -29,35 +32,59 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        HorizontalLayout hLayout = new HorizontalLayout();
+        hLayout.setSpacing(false);
+        hLayout.setMargin(false);
+        hLayout.setSizeFull();
+
+        Label editorLbl = new Label("<strong>Code Editor</strong>", ContentMode.HTML);
+
         final VerticalLayout layout = new VerticalLayout();
+        final VerticalLayout consolePanelLayout = new VerticalLayout();
+        consolePanelLayout.setSpacing(false);
+
+        final VerticalLayout consoleLayout = new VerticalLayout();
+
+        Panel consolePanel = new Panel("Console");
+        consolePanel.setHeight(500.0f, Unit.PIXELS);
 
         TextArea editor = new TextArea();
         editor.setSizeFull();
+        editor.setWidth(100.0f, Unit.PERCENTAGE);
+        editor.setHeight(300.0f, Unit.PIXELS);
         editor.addValueChangeListener(event -> {
            editorInput = String.valueOf(event.getValue());
         });
 
-        Button button = new Button("Click Me");
+        Button button = new Button("Compile code");
         button.addClickListener(e -> {
             layout.addComponent(new Label(editorInput));
 
             CharStream charStream = CharStreams.fromString(editorInput);
             decafLexer decafLexer = new decafLexer(charStream);
             decafLexer.removeErrorListeners();
-            decafLexer.addErrorListener(CustomErrorListener.INSTANCE);
+            decafLexer.addErrorListener(new CustomErrorListener(consolePanelLayout));
             CommonTokenStream commonTokenStream = new CommonTokenStream(decafLexer);
             decafParser decafParser = new decafParser(commonTokenStream);
             decafParser.removeErrorListeners();
-            decafParser.addErrorListener(CustomErrorListener.INSTANCE);
+            decafParser.addErrorListener(new CustomErrorListener(consolePanelLayout));
 
             ParseTree parseTree = decafParser.program();
             layout.addComponent(new Label(parseTree.toStringTree(decafParser)));
 
+            Notification notification = new Notification("Compilation done!", "Execution terminated!");
+            notification.setDelayMsec(2000);
+            notification.setPosition(Position.BOTTOM_LEFT);
+            notification.show(Page.getCurrent());
+
         });
+
+        consolePanel.setContent(consolePanelLayout);
+        consoleLayout.addComponent(consolePanel);
+        layout.addComponents(editorLbl, editor, button);
+        hLayout.addComponents(layout, consoleLayout);
         
-        layout.addComponents(editor, button);
-        
-        setContent(layout);
+        setContent(hLayout);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
